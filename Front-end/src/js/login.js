7c135842adaaa02adf837abdf31f5a1085f870d2
naolca <firaolibrahim28@gmail.com>
2023-02-12 17:09:47 +0300

    boliler plate for the login request

--- Front-end/src/js/login.js
+++ Front-end/src/js/login.js
@@ -1,1 +1,18 @@
+const API = `http://localhost:3000/books/`;
 
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let userName = document.getElementById("user-name").value;
+  let passWord = document.getElementById("password").value;
+
+  fetch(
+    API,
+    {
+      method: "POST",
+      body: JSON.stringify({ username: userName, password: passWord }),
+    }
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+  );
+});
