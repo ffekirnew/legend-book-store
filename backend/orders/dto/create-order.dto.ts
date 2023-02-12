8f5aae20eb292f62375741eef102b40fd109e5bb
Biruk Worku <120275057+Biruk-Worku-Molla@users.noreply.github.com>
2023-02-12 23:03:02 +0300

    added the files for the order feature

---
+++ backend/orders/dto/create-order.dto.ts
@@ -0,0 +1,7 @@
+export class CreateOrderDto {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  location: string;
+  bookId: number;
+}
---
+++ backend/orders/orders.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { OrdersController } from './orders.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { BooksModule } from 'src/books/books.module';
+import { BooksController } from 'src/books/books.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Order]),
+    BooksModule,
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService]
+})
+export class OrdersModule {}
+
